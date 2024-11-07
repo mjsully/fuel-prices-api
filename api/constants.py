3639a741165e7c18df1186b782a0d1d408d7646d
Matt Sullivan <mattys940@gmail.com>
2024-11-07 11:45:40 +0000

    Fix bug with USER_AGENT_HEADERS constant

--- api/constants.py
+++ api/constants.py
@@ -5,7 +5,4 @@ NO_DATA_STRING = "No data available."
 FUELTYPE_ERROR_STRING = """
     {} is not a valid option for fueltype. Please choose one of 'e5', 'e10', 'b7' or 'sdv'."""
 TIMESTAMP_FORMAT = "%H:%M (%d/%m/%Y)"
-USER_AGENT_HEADERS = """
-    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) 
-    AppleWebKit/537.36 (KHTML, like Gecko) 
-    Chrome/128.0.0.0 Mobile Safari/537.36"""
+USER_AGENT_HEADERS = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Mobile Safari/537.36"
